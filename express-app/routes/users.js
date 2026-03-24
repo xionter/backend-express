30b6e292a17cd5c90cfe6b39973d445f477a0a9a
xionter <andrey0502255@gmail.com>
2026-03-24 17:41:53 +0500

    db added

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,48 +1,54 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose()
+
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
+
 const router = express.Router();
 
-const users = []
-users.push(
-    {
-        id: 1,
-        name: "name"
-    },
-    {
-        id: 2,
-        name: "anton"
-    },
-    {
-        id: 3,
-        name: "gus amogu"
-    }
-)
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.json({
-        items: users
+    db.all("SELECT id, name FROM users", function(err, rows) {
+        if (err) {
+            return res.status(404).json({ message: "no users in db" });
+            console.log(err);
+        } else {
+            res.json({ items: rows });
+        }
     })
 });
 
 router.post('/', function(req, res, next) {
-    const lastId = users[users.length - 1].id;
-    const newUser = {
-        id: lastId + 1,
-        name : req.body.name
+    const name = req.body.name
+    if (!name) {
+        return res.status(400).json({ error: "name is required" });
     }
 
-    users.push(newUser)
-    res.status(201).json(newUser);
+    const insert = "INSERT INTO users (name) VALUES (?)";
+    db.run(insert, [name], function(err) {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: "db error" });
+        }
+
+        res.status(201).json({
+            id: this.lastID,
+            name: name
+        });
+    });
 });
 
 router.get('/:id', function(req, res, next) {
-    const userId = parseInt(req.params.id);
-    const user = users.find(u => u.id === userId);
-
-    if (!user) {
-        return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json(user);
+    const id = req.params.id
+    db.get("SELECT id, name FROM users WHERE id = ?", [id], (err, rows) => {
+        if (err) {
+            console.log(err);
+            return res.status(404).json({ message: "User not found" });
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
 module.exports = router;
