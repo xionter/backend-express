4727edac39c64efd7e34223be5bed98057eb92c7
xionter <andrey0502255@gmail.com>
2026-03-24 17:11:09 +0500

    made /users handler

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const users = []
+users.push(
+    {
+        id: 1,
+        name: "name"
+    },
+    {
+        id: 2,
+        name: "anton"
+    },
+    {
+        id: 3,
+        name: "gus amogu"
+    }
+)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+    res.json({
+        items: users
+    })
 });
 
 module.exports = router;
